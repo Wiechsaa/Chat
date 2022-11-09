3054bbc370ee76f7159d4edb11c10143f5a2aee2
SzymonSudul <98656443+Wiechsaa@users.noreply.github.com>
2022-11-09 09:52:06 +0100

    Rooms controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'users', to: 'devise/sessions#new'
   end
+
+  get 'user/:id', to: 'users#show', as: 'user'
+  resources :rooms
 end
