ec09e95aebdc184ccec9fcf9413f3353fbec1a0a
SzymonSudul <98656443+Wiechsaa@users.noreply.github.com>
2022-11-09 21:24:59 +0100

    Default avatar. Avatar in registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,28 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :messages, dependent: :destroy
+  has_one_attached :avatar
 
   scope :all_except, ->(user) { where.not(id: user.id) }
 
   after_create_commit { broadcast_append_to 'users' }
+  after_commit :add_default_avatar, on: %i[create update]
+
+  def avatar_thumbnail
+    avatar.variant(resize_to_limit: [150, 150]).processed
+  end
+
+  def chat_thumbnail
+    avatar.variant(resize_to_limit: [50, 50]).processed
+  end
+
+  private
+
+  def add_default_avatar
+    return if avatar.attached?
+
+    avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default_avatar.png')),
+                  filename: 'default_avatar.png',
+                  content_type: 'image/png')
+  end
 end
